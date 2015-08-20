30924ddc59b4df1a89bb44e318a869e92a954409
nexus <nexus@innotree.com>
2015-08-20 21:59:09 +0900

    3

--- ch02-jpa-start1/src/main/java/jpabook/start/JpaAuthMain.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/JpaAuthMain.java
@@ -50,7 +50,7 @@ public class JpaAuthMain {
         List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
         System.out.println("members.size=" + members.size());
 
-        //삭제....1
+        //삭제....12
         em.remove(member);
     }
 }
